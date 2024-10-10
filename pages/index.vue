c08c241aa66caccde81b60666d221be97b0519a7
JD Solanki <jdsolanki0001@gmail.com>
2024-10-10 14:59:01 +0530

    feat: modified index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
-  <p>Welcome!</p>
+  <div class="grid min-h-screen place-items-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-semibold text-slate-800">
+        NuxtPress
+      </h1>
+      <p class="my-4">
+        🚧 VitePress inspired Nuxt Content theme with compatibility for Nuxt Studio
+      </p>
+      <NuxtLink to="/docs/introduction" class="underline">
+        View Docs
+      </NuxtLink>
+    </div>
+  </div>
 </template>
